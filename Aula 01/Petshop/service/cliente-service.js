acc0f7733e2ddfe220837d586000046f6e9bb535
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-08-22 22:30:24 -0300

    2º commit
    Alteração da página lista_cliente e inserção do template literals no cliente-service.

--- Aula 01/Petshop/service/cliente-service.js
+++ Aula 01/Petshop/service/cliente-service.js
@@ -1,3 +1,21 @@
+const criaNovaLinha = (nome, email) => {
+  const linhaNovoCliente = document.createElement("tr");
+
+  const conteudo = `
+<td class="td" data-td>${nome}</td>
+                <td>${email}</td>
+                <td>
+                    <ul class="tabela__botoes-controle">
+                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                    </ul>
+                </td>`;
+  linhaNovoCliente.innerHTML = conteudo;
+  return linhaNovoCliente;
+};
+
+const tabela = document.querySelector("[data-tabela]");
+
 const http = new XMLHttpRequest(); // Objeto que fornece alguns métodos para fazer a comunicação entre a aplicação e a API
 
 http.open("GET", "http://localhost:3000/profile"); //abre a comunicação entre a minha aplicação e a API.
@@ -5,6 +23,8 @@ http.open("GET", "http://localhost:3000/profile"); //abre a comunicação entre 
 http.send();
 
 http.onload = () => {
-  const data = http.response;
-  console.log(data);
+  const data = JSON.parse(http.response);
+  data.forEach((elemento) => {
+    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+  });
 };
